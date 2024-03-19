c020435ffe37587bea0b041f111089c6bf2ef2ee
MrDuy2312 <d.vo23121999@gmail.com>
2024-03-19 04:41:06 +0000

    2nd commit

---
+++ exercise_75/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+    int Nhap;
+    int p;
+    printf("Nhap n:");
+    scanf("%d", &Nhap);
+    int max = 10000;
+    for(int i = 1; p < n; i++){
+        p*=2;
+    }
+    if(p == n){
+        printf("la so nguyen 4 byte co dang 2^k\n")
+    }
+    else{
+        printf("la so nguyen 4 byte khong co dang 2^k\n")
+    }
+    return 0;
+}
