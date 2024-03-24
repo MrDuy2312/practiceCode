a34aa19186fb53d8119597a2a3a0a97f33ac6f37
MrDuy2312 <d.vo23121999@gmail.com>
2024-03-24 06:18:56 +0000

    fix exercise_75

--- exercise_75/main.c
+++ exercise_75/main.c
@@ -3,19 +3,19 @@
 
 
 int main(){
-    int Nhap;
+    int n;
     int p;
     printf("Nhap n:");
-    scanf("%d", &Nhap);
+    scanf("%d", &n);
     int max = 10000;
     for(int i = 1; p < n; i++){
         p*=2;
     }
     if(p == n){
-        printf("la so nguyen 4 byte co dang 2^k\n")
+        printf("la so nguyen 4 byte co dang 2^k\n");
     }
     else{
-        printf("la so nguyen 4 byte khong co dang 2^k\n")
+        printf("la so nguyen 4 byte khong co dang 2^k\n");
     }
     return 0;
 }
